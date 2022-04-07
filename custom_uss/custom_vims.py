a7d58fb3869759265daeb95f465a54f8adf01383
Joan Hernandez <joanhf98@gmail.com>
2022-04-07 17:54:14 +0200

    corrections

--- custom_uss/custom_vims.py
+++ custom_uss/custom_vims.py
@@ -2,7 +2,6 @@
 
 import json
 import uuid
-
 from flask_socketio import SocketIO
 
 from datetime import datetime, timedelta
@@ -12,101 +11,158 @@ from vertiport import Vertiport
 
 
 class VIMS():
-    """
-    VIMS Management
-    """
-    def __init__(self, _id, _port):
-        self.id = _id
-        self.read_token = None
-        self.write_token = None
-        self.read_headers = None
-        self.write_headers = None
-        self.vertiports = []
-        print("VIMS %s created" % self.id)
+	"""
+	VIMS Management
+	"""
+	def __init__(self, _id, _port):
+		self.id = _id
+		self.read_token = None
+		self.write_token = None
+		self.read_headers = None
+		self.write_headers = None
+		self.vertiports = []
+		print("VIMS %s created" % self.id)
 
-        self.port = _port
-        self.app = Flask(__name__)
+		self.port = _port
+		self.app = Flask(__name__)
 
-        self.socketio = SocketIO(self.app)
+		self.socketio = SocketIO(self.app)
 
-        @self.app.route("/%s" % self.id, methods=['GET'])
-        def homepage():
-            return ("VIMS HOMEPAGE")
-    
-    
-    def authentify_read(self):
-        """
-        Get the token for reading requests.
-        """
-        params = (
-            ('sub', self.id),
-            ('intended_audience', 'localhost'),
-            ('scope', 'vims.read.vertiports'),
-            ('issuer', 'dummy_oauth'),
-        )
+		@self.app.route("/%s" % self.id, methods=['GET'])
+		def homepage():
+			return ("VIMS HOMEPAGE")
+	
+	
+	def authentify_read(self):
+		"""
+		Get the token for reading requests.
+		"""
+		params = (
+			('sub', self.id),
+			('intended_audience', 'localhost'),
+			('scope', 'vims.read.vertiports'),
+			('issuer', 'dummy_oauth'),
+		)
 
-        response = request.get('http://localhost:8085/token', params=params)
-        print(response.json())
+		response = request.get('http://localhost:8085/token', params=params)
+		print(response.json())
 
-        if response.status_code == 200:
-            self.read_token = response.json()["access_token"]
-            self.read_headers = {
-                'Authorization': 'Bearer %s' % self.read_token,
-                'Content-Type': 'application/json'
-            }
-            print("VIMS %s auth read with token %s" % (self.id, self.read_token))
-        else:
-            print("Error in auth read process %ss" % response.text)
+		if response.status_code == 200:
+			self.read_token = response.json()["access_token"]
+			self.read_headers = {
+				'Authorization': 'Bearer %s' % self.read_token,
+				'Content-Type': 'application/json'
+			}
+			print("VIMS %s auth read with token %s" % (self.id, self.read_token))
+		else:
+			print("Error in auth read process %ss" % response.text)
 
-        return response.status_code
+		return response.status_code
 
-    def authentify_write(self):
-        """
-        Get the token for writing requests.
-        """
-        params = (
-            ('sub', self.id),
-            ('intended_audience', 'localhost'),
-            ('scope', 'vims.write.vertiports'),
-            ('issuer', 'dummy_oauth'),
-        )
+	def authentify_write(self):
+		"""
+		Get the token for writing requests.
+		"""
+		params = (
+			('sub', self.id),
+			('intended_audience', 'localhost'),
+			('scope', 'vims.write.vertiports'),
+			('issuer', 'dummy_oauth'),
+		)
 
-        response = request.get('http://localhost:8085/token', params=params)
-        print(response.json())
+		response = request.get('http://localhost:8085/token', params=params)
+		print(response.json())
 
-        if response.status_code == 200:
-            self.write_token = response.json()["access_token"]
-            self.write_headers = {
-                'Authorization': 'Bearer %s' % self.write_token,
-                'Content-Type': 'application/json'
-            }
-            print("VIMS %s auth write with token %s" % (self.id, self.write_token))
-        else:
-            print("Error in auth write process %" % response.text)
+		if response.status_code == 200:
+			self.write_token = response.json()["access_token"]
+			self.write_headers = {
+				'Authorization': 'Bearer %s' % self.write_token,
+				'Content-Type': 'application/json'
+			}
+			print("VIMS %s auth write with token %s" % (self.id, self.write_token))
+		else:
+			print("Error in auth write process %" % response.text)
 
-        return response.status_code
+		return response.status_code
 
-    """
-    VERTIPORT METHODS.
-    """
-    
-    # def check_permission(self)
+	"""
+	VERTIPORT METHODS.
+	"""
+	def create_vertiport_test_ENAC(self):
+		"""
+		Create a predetermined Vertiport "ENAC" for testing.
+		"""
+		f = open("ENAC_Vertiport.json", "r")
+		data = json.load(f)
+		print(data)
 
-    # def update_vertiport
+		_vertiport_id = uuid.uuid1()
+		_time_start = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+		_time_end = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    # def subscribe_vertiport
+		vertiport = Vertiport(_vertiport_id, data[1], data[3], data[4], data[5], data[6], _time_start, _time_end, data[9], data[10], data[11], data[12], data[13])
+		self.vertiports.append(vertiport)
 
-    # def unsubscribe_vertiport
+		print("Vertiport created with id: %s" % _vertiport_id)
+		print(vertiport)
 
-    def get_vertiport(self, _vertiport_id):
-        """
-        Get Vertiport details by its ID.
-        """
-        url = "http://localhost:8082/v1/vertiports/%s" % _vertiport_id
-        response = request.get(url, headers=self.read_headers)
-        print(response.json())
 
-        print("VIMS %s attempting to get Vertiport %s" % (self.id, _vertiport_id))
-        print(response.text)
-        
-        return response
+	def create_vertiport_test_LFBO(self):
+		"""
+		Create a predetermined Vertiport "LFBO" for testing.
+		"""
+		f = open("LFBO_Vertiport.json", "r")
+		data = json.load(f)
+		print(data)
+
+		_vertiport_id = uuid.uuid1()
+		_time_start = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+		_time_end = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+		vertiport = Vertiport(_vertiport_id, data[1], data[3], data[4], data[5], data[6], _time_start, _time_end, data[9], data[10], data[11], data[12], data[13])
+		self.vertiports.append(vertiport)
+
+		print("Vertiport created with id: %s" % _vertiport_id)
+		print(vertiport)
+
+
+	def delete_vertiport(self, _vertiport_name = None):
+		"""
+		Delete a vertiport.
+		"""
+		try:
+			print("Attempting to delete Vertiport from VIMS database")
+
+			if _vertiport_name is not None:
+				vertiport = next(vertiport for vertiport in self.vertiports if str(vertiport.name) == str(_vertiport_name))
+				vertiport_id = vertiport.id
+			del vertiport
+			print("Vertiport %s deleted from local VIMS database" % vertiport_id)
+		except StopIteration:
+			print("Vertiport not existing in VIMS database")
+	
+
+	# def check_permission(self)
+
+
+	# def update_vertiport
+
+
+	# def subscribe_vertiport
+
+
+	# def unsubscribe_vertiport
+
+
+	def get_vertiport(self, _vertiport_id):
+		"""
+		Get Vertiport details by its ID.
+		"""
+		url = "http://localhost:8082/v1/vertiports/%s" % _vertiport_id
+		response = request.get(url, headers=self.read_headers)
+		print(response.json())
+
+		print("VIMS %s attempting to get Vertiport %s" % (self.id, _vertiport_id))
+		print(response.text)
+		
+		return response
--- custom_uss/vims_main.py
+++ custom_uss/vims_main.py
@@ -24,58 +24,6 @@ def is_uuid(uuid):
 	else:
 		return True
 
-def create_vertiport_test_ENAC(self):
-	"""
-	Create a predetermined Vertiport "ENAC" for testing.
-	"""
-	f = open("ENAC_Vertiport.json", "r")
-	data = json.load(f)
-	print(data)
-
-	_vertiport_id = uuid.uuid1()
-	_time_start = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-	_time_end = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-	vertiport = Vertiport(_vertiport_id, data[1], data[3], data[4], data[5], data[6], _time_start, _time_end, data[9], data[10], data[11], data[12], data[13])
-	self.vertiports.append(vertiport)
-
-	print("Vertiport created with id: %s" % _vertiport_id)
-	print(vertiport)
-
-
-def create_vertiport_test_LFBO(self, ):
-	"""
-	Create a predetermined Vertiport "LFBO" for testing.
-	"""
-	f = open("LFBO_Vertiport.json", "r")
-	data = json.load(f)
-	print(data)
-
-	_vertiport_id = uuid.uuid1()
-	_time_start = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-	_time_end = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-	vertiport = Vertiport(_vertiport_id, data[1], data[3], data[4], data[5], data[6], _time_start, _time_end, data[9], data[10], data[11], data[12], data[13])
-	self.vertiports.append(vertiport)
-
-	print("Vertiport created with id: %s" % _vertiport_id)
-	print(vertiport)
-
-
-def delete_vertiport(self, _vertiport_name = None):
-	"""
-	Delete a vertiport.
-	"""
-	try:
-		print("Attempting to delete Vertiport from VIMS database")
-
-		if _vertiport_name is not None:
-			vertiport = next(vertiport for vertiport in self.vertiports if str(vertiport.name) == str(_vertiport_name))
-			vertiport_id = vertiport.id
-		del vertiport
-		print("Vertiport %s deleted from local VIMS database" % vertiport_id)
-	except StopIteration:
-		print("Vertiport not existing in VIMS database")
 
 def main():	
 	"""
@@ -100,15 +48,8 @@ def main():
 		try:
 			cmd = input('\n')
 			cmd_args = re.split(r'\s', cmd)
-			if cmd_args[0] == "create_vertiport":
-				if cmd_args[1] == 'ENAC':
-					vims.create_vertiport_test_ENAC()
-				if cmd_args[1] == 'LFBO':
-					vims.create_vertiport_test_LFBO()
-			elif cmd_args[0] == "get_vertiport":
+			if cmd_args[0] == "get_vertiport":
 				vims.get_vertiport(cmd_args[1])
-			elif cmd_args[0] == "delete_vertiport": 
-				vims.delete_vertiport(_vertiport_name=cmd_args[1])
 		
 		except KeyboardInterrupt:
 			# delete all ISAs created 
@@ -118,5 +59,5 @@ def main():
 			print("Done")
 			return
 
-if __name__ == "__main__":	
+if __name__ == "__main__":
 	main()
